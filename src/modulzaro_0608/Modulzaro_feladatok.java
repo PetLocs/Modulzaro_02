c37dcd5935a184958651ebf8be6de849da6685d1
Yclaze <teralocsi@gmail.com>
2024-06-08 08:51:42 +0200

    no komment, név első sorban

--- src/modulzaro_0608/Modulzaro_feladatok.java
+++ src/modulzaro_0608/Modulzaro_feladatok.java
@@ -1,20 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+//Locsmándi Péter
 package modulzaro_0608;
 
-/**
- *
- * @author Péter
- */
 public class Modulzaro_feladatok {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
