7b06e968ecc579d7766d80ca4a4eb790b8bd2083
Yclaze <teralocsi@gmail.com>
2024-06-08 08:48:56 +0200

    Modulzaro_feladatok

---
+++ src/modulzaro_0608/Modulzaro_feladatok.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modulzaro_0608;
+
+/**
+ *
+ * @author Péter
+ */
+public class Modulzaro_feladatok {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
