effc8112791ed6837b2cdb73a91cb99f52610aff
Yclaze <teralocsi@gmail.com>
2024-06-08 08:54:31 +0200

    psvm, jatek

--- src/modulzaro_0608/Modulzaro_jatek.java
+++ src/modulzaro_0608/Modulzaro_jatek.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+//Locsmándi Péter
 package modulzaro_0608;
 
-/**
- *
- * @author Péter
- */
 public class Modulzaro_jatek {
-    
+    public static void main(String[] args) {
+        
+    }
 }
