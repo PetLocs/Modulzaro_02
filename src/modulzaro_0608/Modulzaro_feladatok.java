8646a5f19b99e9d5709037b5fef25b5b7c1ddcf5
Yclaze <teralocsi@gmail.com>
2024-06-08 09:10:11 +0200

    void feltolt(), int velSzam, tomb, also, felso field

--- src/modulzaro_0608/Modulzaro_feladatok.java
+++ src/modulzaro_0608/Modulzaro_feladatok.java
@@ -3,17 +3,30 @@ package modulzaro_0608;
 
 public class Modulzaro_feladatok {
 
+    static int[] tomb;
+    static int also = 0;
+    static int felso = 10;
+    
     public static void main(String[] args) {
         programozoiTetelek();
         adatszerkezetTomb();
     }
 
     private static void programozoiTetelek() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        feltolt();
+    }
+
+    private static void feltolt() {
+        tomb = new int[velSzam(also, felso)];
+    }
+
+    private static int velSzam(int also, int felso) {
+        return 0;
     }
 
     private static void adatszerkezetTomb() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
+
     
 }
