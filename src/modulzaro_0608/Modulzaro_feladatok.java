e1db2493740857e4a1db65ed37758fef61adfd34
Yclaze <teralocsi@gmail.com>
2024-06-08 08:57:31 +0200

    void programozoiTetelek(), void adatszerkezetTomb(), feladatok

--- src/modulzaro_0608/Modulzaro_feladatok.java
+++ src/modulzaro_0608/Modulzaro_feladatok.java
@@ -4,7 +4,16 @@ package modulzaro_0608;
 public class Modulzaro_feladatok {
 
     public static void main(String[] args) {
-        
+        programozoiTetelek();
+        adatszerkezetTomb();
+    }
+
+    private static void programozoiTetelek() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void adatszerkezetTomb() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
