e4334883ea2c8a71d8142f49da1c6478baf7500b
Yclaze <teralocsi@gmail.com>
2024-06-08 08:53:24 +0200

    Modulzaro_jatek

---
+++ src/modulzaro_0608/Modulzaro_jatek.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modulzaro_0608;
+
+/**
+ *
+ * @author Péter
+ */
+public class Modulzaro_jatek {
+    
+}
