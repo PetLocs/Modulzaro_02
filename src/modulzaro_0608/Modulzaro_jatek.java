b0c8859866cd36f60e235682ed854c23cd994576
Yclaze <teralocsi@gmail.com>
2024-06-08 11:57:44 +0200

    X, O, URES field

--- src/modulzaro_0608/Modulzaro_jatek.java
+++ src/modulzaro_0608/Modulzaro_jatek.java
@@ -2,8 +2,11 @@
 package modulzaro_0608;
 
 public class Modulzaro_jatek {
+    static final String X = "X";
+    static final String O = "O";
+    static final String URES = " ";
+    static String [] mezo = new String[] {X, X, X, URES, O, O, O};
     
-    static String [] mezo = new String[7];
     public static void main(String[] args) {
         
     }
