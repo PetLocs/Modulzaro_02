f1a2fc318cda302d6dc43dfb59cf4b6869ad74f0
Yclaze <teralocsi@gmail.com>
2024-06-08 14:14:18 +0200

    mod lepes()

--- src/modulzaro_0608/Modulzaro_jatek.java
+++ src/modulzaro_0608/Modulzaro_jatek.java
@@ -9,7 +9,6 @@ public class Modulzaro_jatek {
     static int [] magyarazat = new int[] {1,2,3,4,5,6,7};
     static String [] mezok = new String[] {X, X, X, X, X, URES, O, O, O, O, O};
     static final Scanner SC = new Scanner(System.in);
-    static boolean elso = true;
     static String csapat;
     static int mezo;
     
@@ -35,18 +34,11 @@ public class Modulzaro_jatek {
             System.out.printf("%s ", mezok[i]);
         }
         System.out.printf("\n");
-        if (!elso) {
-            System.out.println(csapat + " jön");            
-        }
-   
     }
 
     private static void bekeres() {        
-        if (elso) {
-            System.out.print("Kérem válasszon csapatot!");            
-            csapat = SC.next();
-            elso = false;
-        }        
+        System.out.print("Kérem válasszon csapatot!");            
+        csapat = SC.next();                
         System.out.print("Kérem válasszon mezőt!");
         mezo = SC.nextInt() +2; 
         lepes();
@@ -54,22 +46,22 @@ public class Modulzaro_jatek {
     }
 
     private static void lepes() {        
-        if (mezok[mezo] == URES && mezok[mezo - 1] == X && mezo < 7) {
+        if (mezok[mezo] == URES && mezok[mezo - 1] == X) {
             mezok[mezo - 1] = URES;
             mezok[mezo] = X;
             csapat = O;
         }
-        if (mezok[mezo] != URES && mezok[mezo + 1] == URES && mezok[mezo - 1] == X && mezo < 6) {
+        if (mezok[mezo] != URES && mezok[mezo + 1] == URES && mezok[mezo - 1] == X) {
             mezok[mezo - 1] = URES;
             mezok[mezo + 1] = X;
             csapat = O;
         }
-        if (mezok[mezo - 2] == URES && mezok[mezo - 1] == O && mezo > 2) {
+        if (mezok[mezo - 2] == URES && mezok[mezo - 1] == O) {
             mezok[mezo - 1] = URES;
             mezok[mezo - 2] = O;
             csapat = X;
         }
-        if (mezok[mezo - 2] != URES && mezok[mezo - 3] == URES && mezok[mezo - 1] == O && mezo > 3) {
+        if (mezok[mezo - 2] != URES && mezok[mezo - 3] == URES && mezok[mezo - 1] == O) {
             mezok[mezo - 1] = URES;
             mezok[mezo - 3] = O;
             csapat = X;
